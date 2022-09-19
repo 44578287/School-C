dde7246838351415c5d83b5e584dcfe5db8da680
智捷 游 <g9964957@gmail.com>
2022-09-19 15:14:04 +0800

    修改非上传指定文件 作业2.cpp

---
+++ c/2022/09/19/2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int num = 5;
+
+    printf("111104209 游智捷\n");
+    printf("num/2=%d\n", num / 2);               /* 整數相除 */
+    printf("(float)num/2=%f\n", (float)num / 2); /* 將整數轉成浮點數，再做除法 */
+}
