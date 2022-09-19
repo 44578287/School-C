a43a4a214ed90b5d58a58512e9ae500d37e5f910
智捷 游 <g9964957@gmail.com>
2022-09-19 14:19:18 +0800

    first commit

---
+++ c/2022/09/19/1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    char sum, s = 127;
+
+    printf("111104209 游智捷\n");
+    printf("sum+1= %d\n", sum = s+1);
+    printf("sum+2= %d\n", sum = s+2);
+}
